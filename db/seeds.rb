d096f1f9026beb93f2a66b14df4f4b14ba4e8a0f
valerie schraauwers <valerieschraauwers@gmail.com>
2018-11-20 14:33:51 +0100

    wikinimous-new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(7, true, 10)
+    )
+  article.save
+end
+
